e69737017b7258e45805fa2a26a95423b95b8607
infolog <infolog@LAPTOP-E2MTVL69>
2020-07-17 18:36:20 -0300

    Commit inicial

---
+++ src/calculadora/Magic.java
@@ -0,0 +1,15 @@
+package calculadora;
+
+public class Magic {
+	public static void main(String[] args) {
+int myNumber = 3333;
+		/*We will refer to myNumber as the original number from now on - it might be helpful to document this.*/
+int stepOne = myNumber*myNumber;
+int stepTwo = myNumber+stepOne;
+int stepThree = stepTwo/myNumber;
+int stepFour = stepThree + 17;
+int stepFive = stepFour - myNumber;
+int stepSix = stepFive/6;
+System.out.println(stepSix);
+	}
+}
